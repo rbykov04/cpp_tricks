ec5c4986577f9115c673075f8fee115b0302505b
rbykov <rbykov04@gmail.com>
2019-10-08 00:03:22 +0300

    fix merge alg

--- src/forward_list_wrapper.cpp
+++ src/forward_list_wrapper.cpp
@@ -5,8 +5,14 @@ template<class NODE, class VALUE>
 struct forward_list_wrapper{
     NODE *head;
     NODE *tail;
+    typedef  forward_list_wrapper<NODE, VALUE> conctainer_type;
     forward_list_wrapper():head(0), tail(0){
         
+    }
+    forward_list_wrapper(std::initializer_list<VALUE> l):head(0), tail(0){
+    	for (auto i: l){
+    		push_back(i);
+    	}
     }
     forward_list_wrapper(NODE* _head):head(_head), tail(0){
         
@@ -63,6 +69,30 @@ struct forward_list_wrapper{
         }
         head = d;
     }
+    void make_tail(){
+    	if (head){
+        	tail = head;
+        	while(tail->next){
+        		tail = tail->next;
+        	}
+        }
+    }
+    void concat(conctainer_type& a){
+    	if (!head){
+    		head = a.head;
+    		tail = a.tail;    	
+    		a.head = 0;
+    		a.tail = 0;
+    		return;
+    	}
+    	if (!tail){
+    		make_tail();
+    	}
+    	tail->next = a.head;
+    	tail = a.tail;
+    	a.head = 0;
+    	a.tail = 0;
+    }
     struct iterator: public std::iterator<
 						std::input_iterator_tag,   // iterator_category
 						NODE,                      // value_type
@@ -112,30 +142,39 @@ template<class NODE, class VALUE>
 void merge_from_sort_list(forward_list_wrapper<NODE, VALUE>& a, forward_list_wrapper<NODE, VALUE>& b){
 	auto current = std::begin(a);
 	while (std::begin(b) != std::end(b)){
-		auto after = current;
-		while(after != std::end(a) && *after < *std::begin(b)){
+		auto after =  current;
+		while(after != std::end(a) && *after <= *std::begin(b)){
 			current = after;
 			after++;
 		}
 		
-		if (current == std::end(a)){
-			a.push_back(b.head);
-			b.head = 0;
-			b.tail = 0;
+		if (after == std::end(a)){
+			a.concat(b);
 			return;
 		}
 
 		NODE * head = b.slice_head();
-		// std::cout << *current <<"add slice "<<head->val<<"\n";
-		if (current == std::begin(a) && head->val < *current){
+		if (after == std::begin(a)){
+			std::cout << head->val <<" push_front\n";
 			a.push_front(head);
-		}else{
+			current = std::begin(a);
 
-			a.insert_after(after, head);
+		}else{
+			std::cout << head->val <<" insert_after"<<*current<<"\n";
+			a.insert_after(current, head);
+
 		}
 		// current++;
 
 	}
+}
+template<class NODE, class VALUE>
+std::ostream& operator<<(std::ostream& os, forward_list_wrapper<NODE, VALUE> &list){
+	for (auto v: list){
+		os<<v<<" => ";
+	}
+	os<<"NULL";
+	return os;
 }
 
 struct ListNode {
@@ -150,7 +189,7 @@ int main() {
  	list.push_back(0);
  	list.push_back(4);
  	list.push_back(5);
-
+	
  	forward_list_wrapper<ListNode, int> list2;
  	list2.push_back(1);
  	list2.push_back(3);
@@ -159,14 +198,43 @@ int main() {
  	forward_list_wrapper<ListNode, int> list3;
  	list3.push_back(0);
  	list3.push_back(6);
-	merge_from_sort_list(res, list);
+	std::cout << list <<"  list\n";
+	std::cout << list2 <<"  list2\n";
+	std::cout << list3 <<"  list3\n";
 	merge_from_sort_list(res, list2);
+	merge_from_sort_list(res, list);
+	std::cout << res <<" list1 \n";
+	std::cout << res <<"  :list 1 merge 2\n";
+	std::cout << list3 <<"  list3\n";
  	merge_from_sort_list(res, list3);
-	for(auto v : res) {
-		std::cout << v <<"\n";
-	}
+ 	std::cout << res <<"  :list res merge 3\n";
+ 	{
 
+ 		forward_list_wrapper<ListNode, int> list{0};
+ 		forward_list_wrapper<ListNode, int> list2{1};
 
+		merge_from_sort_list(list, list2);
+ 		std::cout<<list<<"\n";
+ 	} 	{
 
+ 		forward_list_wrapper<ListNode, int> list{1};
+ 		forward_list_wrapper<ListNode, int> list2{0};
 
+		merge_from_sort_list(list, list2);
+ 		std::cout<<list<<"\n";
+ 	} 	{
+
+ 		forward_list_wrapper<ListNode, int> list{1};
+		forward_list_wrapper<ListNode, int>list2;
+		merge_from_sort_list(list, list2);
+ 		std::cout<<list<<"\n";
+ 	}	{
+ 		forward_list_wrapper<ListNode, int> list{-1,1};
+		forward_list_wrapper<ListNode, int>list2{-3,1,4};
+		forward_list_wrapper<ListNode, int>list3{-2,-1,0,2};
+		merge_from_sort_list(list, list2);
+ 		std::cout<<list<<"\n";
+		merge_from_sort_list(list, list3);
+ 		std::cout<<list<<"\n";
+ 	}
 }
