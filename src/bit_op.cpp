22961763f9d7cfcba4f380b4c4e6cbab0ebe8856
rbykov <r.bykov@rotek.ru>
2019-11-01 17:31:49 +0300

    feature: struct for print bit op

--- src/bit_op.cpp
+++ src/bit_op.cpp
@@ -7,18 +7,18 @@
 #include <memory>
 
 struct bit_print{
+    friend std::ostream& operator<<(std::ostream& os, bit_print b);
     unsigned int x;
     unsigned int tabs;
-    bit_print(unsigned int x_, int tabs_=32):x(x_), tabs(tabs_){}
+    bit_print(unsigned int x_, int tabs_=4):x(x_), tabs(tabs_){}
+    void operator=(bit_print v){
+        x = v.x;
+        std::cout<<"--------------------------------------------\n";
+        std::cout<<v<<" new value\n";
+        std::cout<<"--------------------------------------------\n";
+
+    }
 };
-unsigned int revers(unsigned int x){
-    x = (x & 0x55555555) << 1 | (x & 0xAAAAAAAA) >> 1;
-    x = (x & 0x33333333) << 2 | (x & 0xCCCCCCCC) >> 2;
-    x = (x & 0x0F0F0F0F) << 4 | (x & 0xF0F0F0F0) >> 4;
-    x = (x & 0x00FF00FF) << 8 | (x & 0xFF00FF00) >> 8;
-    x = (x & 0x0000FFFF) <<16 | (x & 0xFFFF0000) >> 16;
-    return x;
-}
 std::ostream& operator<<(std::ostream& os, bit_print b){
     for (unsigned int i = 0; i< 32; ++i){
         if (i % b.tabs == 0){
@@ -31,31 +31,58 @@ std::ostream& operator<<(std::ostream& os, bit_print b){
 
     return os;
 }
-unsigned int revers_step_by_step(unsigned int x){
-    std::cout <<bit_print(x)<<" x\n";
+// std::hex<<b.x<<std::dec<<
+bit_print operator&(bit_print b, bit_print v){
+    std::cout<<"    "<<b.x<<"(0x"<<std::hex<<b.x<<")"<<std::dec<<" & 0x"<<v.x<<"(0x"<<std::hex<<v.x<<")"<<std::dec<<"\n";
+    std::cout<<b<<" &\n"<<v<<"\n";
+    b.x&=v.x;
+    std::cout<<b<<" : "<<b.x<<" : "<<std::hex<<b.x<<std::dec<<"\n";
+   // std::cout<<"--------------------------------------------\n";
+    return b;
+}
+bit_print operator<<(bit_print b, bit_print v){
+    std::cout<<b<<" << "<<v.x<<"\n";
+    b.x<<=v.x;
+    std::cout<<b<<" : "<<b.x<<" : "<<std::hex<<b.x<<std::dec<<"\n";
+   // std::cout<<"--------------------------------------------\n";
+    return b;
+}
 
-    std::cout <<bit_print(x, 2)<<" x\n";
+bit_print operator|(bit_print b, bit_print v){
+    std::cout<<"    "<<b.x<<"(0x"<<std::hex<<b.x<<")"<<std::dec<<" | "<<v.x<<"(0x"<<std::hex<<v.x<<")"<<std::dec<<"\n";
+    std::cout<<b<<" |\n"<<v<<"\n";
+    b.x|=v.x;
+    std::cout<<b<<" : "<<b.x<<" : "<<std::hex<<b.x<<std::dec<<"\n";
+   // std::cout<<"--------------------------------------------\n";
+    return b;
+}
+bit_print operator>>(bit_print b, bit_print v){
+    std::cout<<b<<" >> "<<v.x<<"\n";
+    b.x>>=v.x;
+    std::cout<<b<<" : "<<b.x<<" : "<<std::hex<<b.x<<std::dec<<"\n";
+   // std::cout<<"--------------------------------------------\n";
+
+    return b;
+}
+
+bit_print revers(bit_print x){
     x = (x & 0x55555555) << 1 | (x & 0xAAAAAAAA) >> 1;
-    std::cout <<bit_print(x, 2) << " :x = (x & 0x55555555) << 1 | (x & 0xAAAAAAAA) >> 1;\n\n";
-    std::cout <<bit_print(x, 2)<<" x\n";
     x = (x & 0x33333333) << 2 | (x & 0xCCCCCCCC) >> 2;
-    std::cout <<bit_print(x, 2) << " :x = (x & 0x33333333) << 2 | (x & 0xCCCCCCCC) >> 2;\n\n";
-
-     std::cout <<bit_print(x, 4)<<" x\n";
     x = (x & 0x0F0F0F0F) << 4 | (x & 0xF0F0F0F0) >> 4;
-    std::cout <<bit_print(x, 4) << " :x = (x & 0x0F0F0F0F) << 4 | (x & 0xF0F0F0F0) >> 4;\n\n";
-
-     std::cout <<bit_print(x, 8)<<" x\n";
     x = (x & 0x00FF00FF) << 8 | (x & 0xFF00FF00) >> 8;
-    std::cout <<bit_print(x, 8) << " :x = (x & 0x00FF00FF) << 8 | (x & 0xFF00FF00) >> 8;\n\n";
-     std::cout <<bit_print(x, 16)<<" x\n";
     x = (x & 0x0000FFFF) <<16 | (x & 0xFFFF0000) >> 16;
-    std::cout <<bit_print(x, 16) << " :x = (x & 0x0000FFFF) <<16 | (x & 0xFFFF0000) >> 16\n\n";
-    std::cout <<bit_print(x)<<" result revers\n";
     return x;
 }
 
+
 int main(){
-    revers_step_by_step(100000401);
+    // revers(5);
+    bit_print x = 2147516416;
+
+    x = x|(x>>1); 
+    x = x|(x>>2);
+    x = x|(x>>4);
+    x = x|(x>>8);
+    x = x|(x>>16);
     return 0;
 }
